d1b7ea066b6e9ec409d13a0323982cb0c6dd4273
AJRepo <AJRepo@users.noreply.github.com>
2021-01-21 10:42:41 -0600

    find user calling sudo

--- timed_hosts_override.sh
+++ timed_hosts_override.sh
@@ -13,17 +13,25 @@
 BACKUP_FILE="/etc/hosts.bak"
 #Overrides file to be used
 OVERRIDE_FILE="/etc/hosts.overrides"
-MYGROUP=$(groups | awk '{print $1}')
 
-if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run with sudo" 1>&2
+#if [[ $EUID -ne 0 ]]; then
+if [ ! -w /etc/hosts ]; then
+   echo "You do not have write access to /etc/hosts. Recommend calling this script with sudo to avoid sudo timeout." 1>&2
    exit 1
 fi
+
+if [[ $SUDO_USER != "" ]]; then
+  WHOAMI=$SUDO_USER
+else
+  WHOAMI=$(whoami)
+fi
+
+MYGROUP=$(groups "$WHOAMI" | awk '{print $1}')
 
 if [ -e "$BACKUP_FILE" ]; then
   diff -q /etc/hosts $BACKUP_FILE
   if [ $? == 1 ]; then
-    echo "Stopping - backup file exists and is different"
+    echo "Stopping - backup file $BACKUP_FILE exists and is different"
     exit
   fi
 fi
@@ -31,7 +39,7 @@ fi
 sudo cp /etc/hosts $BACKUP_FILE
 
 if [ ! -e "$OVERRIDE_FILE" ]; then
-    echo "Stopping - $OVERRIDE_FILE does not exist"
+    echo "Stopping - $OVERRIDE_FILE does not exist."
     exit
 fi 
 
@@ -47,7 +55,7 @@ echo -e '\a'
 
 #Check if command play exists and if so make a sound
 if [[ $(command -v wall) ]]; then
-  wall $MYGROUP "Time's Up"
+  wall "$MYGROUP" "Time's Up"
 fi
 #if [[ $(command -v play) ]]I; then
 #  play -nq -t alsa synth .5 sine 440
@@ -57,5 +65,5 @@ fi
 sudo mv $BACKUP_FILE /etc/hosts
 
 if [ $? == 1 ]; then
-  wall $MYGROUP "Warning - Error in restoring over $BACKUP_FILE"
+  wall "$MYGROUP" "Warning - Error in restoring over $BACKUP_FILE"
 fi
